39b1109bcf43c827d40578fc534ec1b2ebc4a599
LegendaryThien <anhthientbd@gmail.com>
2025-04-16 00:49:19 -0700

    added pipes for now

--- HW2/myshell.cpp
+++ HW2/myshell.cpp
@@ -22,14 +22,15 @@ exit - Exit the shell
 
 // Maximum number of tokens (arguments) a command can have
 const int MAX_TOKENS = 20;
+const int MAX_COMMANDS = 10;
 
 // Structure to hold a command and its arguments
 struct Command {
     // Array of pointers to command arguments (including the command itself)
     // The last element must be NULL for execvp to work properly
-    char* args[MAX_TOKENS + 1];
+    char* cmds[MAX_TOKENS + 1];
     // Number of arguments in this command
-    int argCount;
+    int cmdCount;
 };
 
 // Function to parse the command line into a command
@@ -41,24 +42,63 @@ void parse(char* commandLine, Command& command) {
     }
     
     // Parse this command into arguments
-    command.argCount = 0;
+    command.cmdCount = 0;
     
     // Split the command by spaces to get individual arguments
     char* token = strtok(commandLine, " ");
     
     // Store each argument in the command structure
-    while (command.argCount < MAX_TOKENS) {
-        command.args[command.argCount++] = token;
+    while (token != nullptr && command.cmdCount < MAX_TOKENS) {
+        command.cmds[command.cmdCount++] = token;
         token = strtok(nullptr, " "); // Get the next token
     }
     
     // Set the last argument to NULL (required by execvp)
-    command.args[command.argCount] = nullptr;
+    command.cmds[command.cmdCount] = nullptr;
+}
+
+// Function to parse a command line with pipes
+void parseWithPipes(char* commandLine, Command commands[], int& cmdCount) {
+    // Remove trailing newline if present
+    size_t len = strlen(commandLine);
+    if (len > 0 && commandLine[len-1] == '\n') {
+        commandLine[len-1] = '\0';
+    }
+    
+    // Split the command line by pipe character
+    char* cmd = strtok(commandLine, "|");
+    cmdCount = 0;
+    
+    while (cmd != nullptr && cmdCount < MAX_COMMANDS) {
+        // Trim leading and trailing whitespace
+        while (*cmd == ' ') cmd++;
+        char* end = cmd + strlen(cmd) - 1;
+        while (end > cmd && *end == ' ') {
+            *end = '\0';
+            end--;
+        }
+        
+        // Parse this command into arguments
+        commands[cmdCount].cmdCount = 0;
+        char* token = strtok(cmd, " ");
+        
+        while (token != nullptr && commands[cmdCount].cmdCount < MAX_TOKENS) {
+            commands[cmdCount].cmds[commands[cmdCount].cmdCount++] = token;
+            token = strtok(nullptr, " ");
+        }
+        
+        // Set the last argument to NULL (required by execvp)
+        commands[cmdCount].cmds[commands[cmdCount].cmdCount] = nullptr;
+        
+        // Get the next command
+        cmd = strtok(nullptr, "|");
+        cmdCount++;
+    }
 }
 
 // Function to execute a command
 void execute(Command& command) {
-    if (command.argCount == 0) return;
+    if (command.cmdCount == 0) return;
     
     // Create a new process to execute the command
     pid_t pid = fork();
@@ -69,9 +109,9 @@ void execute(Command& command) {
         return;
     } if (pid == 0) { // child process
         // Replace the child process with the new program
-        execvp(command.args[0], command.args);
+        execvp(command.cmds[0], command.cmds);
         // If execvp returns, it means the command was not found
-        std::cerr << "Command not found: " << command.args[0] << std::endl;
+        std::cerr << "Command not found: " << command.cmds[0] << std::endl;
         exit(1);
     } else { // parent process
     
@@ -87,11 +127,87 @@ void execute(Command& command) {
     }
 }
 
+// Function to execute piped commands
+void executePipes(Command commands[], int cmdCount) {
+    if (cmdCount == 0) return;
+    
+    // If there's only one command, execute it normally
+    if (cmdCount == 1) {
+        execute(commands[0]);
+        return;
+    }
+    
+    // Create pipes for communication between processes
+    int** pipes = new int*[cmdCount - 1];
+    for (int i = 0; i < cmdCount - 1; i++) {
+        pipes[i] = new int[2];
+        if (pipe(pipes[i]) == -1) {
+            std::cerr << "Pipe creation failed" << std::endl;
+            return;
+        }
+    }
+    
+    // Create child processes for each command
+    for (int i = 0; i < cmdCount; i++) {
+        pid_t pid = fork();
+        
+        if (pid < 0) {
+            std::cerr << "Fork failed" << std::endl;
+            return;
+        } else if (pid == 0) { // Child process
+            // Set up pipes for this child
+            if (i > 0) { // Not the first command
+                // Read from the previous pipe
+                dup2(pipes[i-1][0], STDIN_FILENO);
+                close(pipes[i-1][1]);
+            }
+            
+            if (i < cmdCount - 1) { // Not the last command
+                // Write to the next pipe
+                dup2(pipes[i][1], STDOUT_FILENO);
+                close(pipes[i][0]);
+            }
+            
+            // Close all other pipes
+            for (int j = 0; j < cmdCount - 1; j++) {
+                if (j != i) close(pipes[j][0]);
+                if (j != i - 1) close(pipes[j][1]);
+            }
+            
+            // Execute the command
+            execvp(commands[i].cmds[0], commands[i].cmds);
+            std::cerr << "Command not found: " << commands[i].cmds[0] << std::endl;
+            exit(1);
+        } else { // Parent process
+            // Close the pipes that the parent doesn't need
+            if (i > 0) {
+                close(pipes[i-1][0]);
+                close(pipes[i-1][1]);
+            }
+        }
+    }
+    
+    // Wait for all child processes to complete
+    for (int i = 0; i < cmdCount; i++) {
+        int status;
+        wait(&status);
+    }
+    
+    // Clean up the pipes
+    for (int i = 0; i < cmdCount - 1; i++) {
+        delete[] pipes[i];
+    }
+    delete[] pipes;
+}
+
 int main() {
     // Buffer to store the command line input
     char commandLine[1024];
     // Command structure to store parsed command
     Command command;
+    // Array of commands for piped commands
+    Command commands[MAX_COMMANDS];
+    int cmdCount;
     
     // Print the shell's PID
     std::cout << "Shell (Parent) PID: " << getpid() << std::endl;
@@ -103,16 +219,26 @@ int main() {
         std::cout.flush();
         
         // Read command from stdin
-        fgets(commandLine, sizeof(commandLine), stdin); // removing causes segfault
-        
-        // Parse the command line
-        parse(commandLine, command);
-
-        // Execute the command if there are arguments
-        if (command.argCount > 0) {
-            execute(command);
+        if (fgets(commandLine, sizeof(commandLine), stdin) == nullptr) {
+            // Handle EOF (Ctrl+D)
+            std::cout << std::endl;
+            break;
         }
         
+        // Check if the command contains a pipe
+        if (strchr(commandLine, '|') != nullptr) {
+            // Parse and execute piped commands
+            parseWithPipes(commandLine, commands, cmdCount);
+            if (cmdCount > 0) {
+                executePipes(commands, cmdCount);
+            }
+        } else {
+            // Parse and execute a single command
+            parse(commandLine, command);
+            if (command.cmdCount > 0) {
+                execute(command);
+            }
+        }
     }
     
     return 0;
